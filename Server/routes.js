ca5c122f68a89bf3e0dad424279fdab60e57c3fc
Laura Nunes <laura.a.sousa.nunes@gmail.com>
2024-02-24 19:07:34 -0400

    Adição de uma simples requisição POST '/clientes' no routes.js

--- Server/routes.js
+++ Server/routes.js
@@ -1,7 +1,14 @@
 const express = require("express");
+
+const allClients = [{ nome: "Alex", status: false}];
 const clientsRoutes = express.Router();
 
 //C
+clientsRoutes.post("/clientes", (request, response) => {
+    const {name} = request.body;
+    allClients.push({name, status: false});
+    return response.status(201).json(allClients);
+});
 //R
 //U
 //D
