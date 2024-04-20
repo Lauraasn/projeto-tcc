498a60655d15d6bf76156850cbb429ec90553f6d
Laura Nunes <laura.a.sousa.nunes@gmail.com>
2024-04-19 20:27:13 -0400

    Exibição dos dados do database por meio de alterações no routes.js e App.js

--- Server/routes.js
+++ Server/routes.js
@@ -3,15 +3,20 @@ const pool = require('./db');
 const { join } = require("node:path");
 const schema = require('./schema');
 
-const clientsRoutes = express.Router();
-const filePath = join(__dirname, "schema.sql");
+const routes = express.Router();
+//const filePath = join(__dirname, "schema.sql");
 
-//Create
-clientsRoutes.post("/clientes", async (req, res, next) => {
+//Json que aparece no localhost:8080
+routes.get("/", (req, res) => {
+    res.json("Olá, aqui é o servidor!")
+});
+
+//Create (POST url)
+routes.post("/clientes", async (req, res, next) => {
     //const values = [req.body.name, req.body.age, req.body.sex, req.body.diagnostic, req.body.observation];
     try{
         const results = await pool.query(`INSERT INTO clientes(nome, idade, sexo, diagnostico, observacao) 
-                                          VALUES('Joana', 52, 'F', 'Gordura Localizada', '') 
+                                          VALUES('Amadeu', 70, 'M', 'Parkinson', '') 
                                           ON CONFLICT DO NOTHING`/*, values*/);
         res.json(results.rows);
         console.log("Novo cliente cadastrado com sucesso")
@@ -20,18 +25,17 @@ clientsRoutes.post("/clientes", async (req, res, next) => {
         throw error;
     }
 });
-//Read
-clientsRoutes.get("/clientes", async (req, res, next) => {
+//Read (GET url ou GET url/:id <-- pra ler um específico)
+routes.get("/clientes", async (req, res) => {
     try {
-        const results = await pool.query(`SELECT * FROM clientes`);
-        res.json(results.rows);
-        console.log("Clientes listados com sucesso");
-    } catch (error) {
-        console.error('Encountered an error while executing the query:', error);
-        throw error;
+        const { rows } = await pool.query(`SELECT * FROM clientes`);
+        res.json(rows);
+    } catch (err) {
+        console.error('Erro ao coletar dados do banco de dados:', err);
+        res.status(500).json({error: 'Erro no Servidor Interno'});
     }
 });
-//Update
-//Delete
+//Update (PUT url/:id)
+//Delete (DELETE url/:id)
 
-module.exports = clientsRoutes;
+module.exports = routes;
--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  //useEffect(() => {}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/clientes');
+        setData(response.data);
+      } catch (err) {
+        console.error('Erro ao coletar dados:', err);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Click the "Connect" button to connect with NodeJS
-        </p>
+      <h1>Página principal</h1>
+      <p>É a página que vai ter o CRUD em si</p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <form action="../../post" method="post" className="form">
-          <button type="submit">Connect</button>
-        </form>
-
-      </header>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Ordem</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Idade</th>
+            <th scope="col">Sexo</th>
+            <th scope="col">Diagnóstico</th>
+            <th scope="col">Observação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <th scope="row" key={item.id}>{item.id}</th>
+              <td key={item.id}>{item.nome}</td>
+              <td key={item.id}>{item.idade}</td>
+              <td key={item.id}>{item.sexo}</td>
+              <td key={item.id}>{item.diagnostico}</td>
+              <td key={item.id}>{item.observacao}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
